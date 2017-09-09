742c820a5deb72c182cb36ba9bc7148063bbb0e1
JimieT <jimiet@iqmetrix.com>
2017-09-08 17:18:53 -0700

    Initial ASP.NET template web app code

---
+++ ContactFormEmailer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ContactFormEmailer.MvcApplication" Language="C#" %>
---
+++ ContactFormEmailer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContactFormEmailer.Startup))]
+namespace ContactFormEmailer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
